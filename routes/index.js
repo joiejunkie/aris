c1af80107c47a5cda66c93578bbcfd005a59f117
joiejunky <joiejunky@gmail.com>
2018-07-22 09:48:26 +0100

    Initial_Commit
    Initial commit of charts copied from C:\Users\DLPC0\Documents\Node\ECharts\EChartsRepo.
    Steps 1-5 in the file '\CHEATSHEETS\Node\Node cheatsheet.docx' had already been taken before making this commit

---
+++ routes/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Chart Repository' });
+});
+
+/* GET routers */
+
+/* ---------- BAR CHARTS ----------*/
+
+/* simpleBarChart */
+router.get('/barCharts/simpleBarChart', (req, res, next) => {
+    res.sendFile(__dirname + '/charts/barCharts/simpleBarChart.html');
+});
+/* waterfalls */
+router.get('/barCharts/waterfalls', (req, res, next) => {
+    res.sendFile(__dirname + '/charts/barCharts/waterfalls.html');
+});
+
+/* ---------- HEATMAP ----------*/
+
+/* weeklyHeatmap */
+router.get('/heatmaps/cartesianWeekly', (req, res, next) => {
+    res.sendFile(__dirname + '/charts/heatmaps/cartesianWeekly.html');
+});
+
+/* ---------- LINE CHARTS ----------*/
+
+/* timeSeriesPlot */
+router.get('/lineCharts/timeSeriesPlot', (req, res, next) => {
+    res.sendFile(__dirname + '/charts/lineCharts/timeSeriesPlot.html');
+});
+/* energyUse */
+router.get('/lineCharts/energyUse', (req, res, next) => {
+    res.sendFile(__dirname + '/charts/lineCharts/energyUse.html');
+});
+/* ---------- OTHERS ----------*/
+
+/* pictograph */
+router.get('/others/Pictograph', (req, res, next) => {
+    res.sendFile(__dirname + '/charts/others/pictograph.html');
+});
+
+/* ---------- SCATTERPLOT ----------*/
+
+/* Scatterplots */
+router.get('/scatterPlots/scatterZoom', (req, res, next) => {
+    res.sendFile(__dirname + '/charts/scatterPlots/scatterZoom.html');
+});
+/* menvwomen */
+router.get('/scatterPlots/menvwomen', (req, res, next) => {
+    res.sendFile(__dirname + '/charts/scatterPlots/menvwomen.html');
+});
+
+module.exports = router;
